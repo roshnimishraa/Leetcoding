d5e13ec0e642df1b0e81a3cbfca04e2dca9ea7fa
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-04-05 23:15:37 +0530

    Time: 0 ms (100.00%), Space: 12.6 MB (33.46%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,34 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-    // base case 
-    if(root == NULL){
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        q.push(root); 
+        
+        
+        if(root == NULL){
         return ans;
     }
-        queue<TreeNode*>q;
-        q.push(root);
-    while(!q.empty())
-{
-  vector<int>temp;
-    int size = q.size();
-  if(size == 0){
-      return ans;
-  }
-  while(size>0){
-       TreeNode* curr= q.front();
-  q.pop();
-  temp.push_back(curr->val);
-      if(curr->left != NULL){
-          q.push(curr->left);
-      }
-      if(curr->right != NULL){
-          q.push(curr->right);
-      }
-      size--;
-  }
-ans.push_back(temp);
-}
-return ans;
+        
+        while(!q.empty()){
+            vector<int> temp;
+            int size = q.size();
+            
+        if(size == 0){
+            return ans;
+        }
+            while(size > 0){
+                TreeNode *curr = q.front();
+                q.pop();
+            temp.push_back(curr->val);
+            
+            if(curr ->left != NULL){
+                q.push(curr->left);
+            }
+            if(curr -> right != NULL){
+                q.push(curr -> right);
+            }
+                size--;
+            }
+            ans.push_back(temp);
+        }
+        return ans;
     }
 };
