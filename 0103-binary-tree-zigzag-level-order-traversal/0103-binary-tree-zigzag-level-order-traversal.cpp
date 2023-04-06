f4f7dbf9be3ee44c6d6fc85e58fe3cdd8ae6a5d6
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-04-06 10:46:50 +0530

    Time: 3 ms (71.92%), Space: 12.2 MB (17.40%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,50 +12,36 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-//         store final result
-        vector<vector<int>> ans;
-// storing in queue data structure
+        
+    vector<vector<int>> ans;
         queue<TreeNode*> q;
         q.push(root);
-
-        if(root == NULL) {
-            return ans;
-        }
-// Traversing
-    bool leftToRight = true;
-
-while(!q.empty()){
+    if(root == NULL) return ans;
     
+        bool leftToRight = true;
+    while(!q.empty()){
     int levelSize = q.size();
-
-//     temp array to store level element
-    vector<int> temp(levelSize);
- 
-// Level process
-    for(int i=0;i<levelSize; i++){
-
- TreeNode* frontNode = q.front();
+    vector<int>temp(levelSize);
+    
+    for(int i=0; i<levelSize; i++){
+        TreeNode* frontNode = q.front();
         q.pop();
-
-// Normal or Reverse Traverse
     
-      int index = leftToRight ?  i : levelSize-i-1 ;
+    int index = leftToRight ? i : levelSize - i -1;
         temp[index] = frontNode -> val;
-   
     
-if(frontNode -> left != NULL){
-    q.push(frontNode->left);
-}
-    if(frontNode -> right != NULL){
-        q.push(frontNode -> right);
+    if(frontNode -> left != NULL){
+     q.push(frontNode ->left);
     }
-  }
-//     change direction
-    leftToRight = !leftToRight;
-     ans.push_back(temp);
-    
-}
-       
-return ans;
-}
+    if(frontNode ->right != NULL) {
+        q.push(frontNode ->right);
+    }
+
+    }
+        //     direction change krdo
+        leftToRight = !leftToRight;
+    ans.push_back(temp);
+    }
+        return ans;
+    }
 };
