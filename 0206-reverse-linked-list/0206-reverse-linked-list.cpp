ba93e1ba0f6e0f9832cdb09f484dc67f2354cfd4
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-04-06 11:16:03 +0530

    Time: 5 ms (71.95%), Space: 8.5 MB (19.70%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,22 +11,17 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next==NULL) 
+            return head;
     
-//  either head is empty or there is only one node
-  if(head == NULL || head->next == NULL){
-      return head;
-  }      
-// rest_head is last node 
-    ListNode* rest_head = reverseList(head -> next);
-// head next for the purpose of change direction       
-ListNode* rest_tail =head->next;
+        ListNode* rest_head = reverseList(head->next);
+        ListNode* rest_tail = head->next;
     
-//        change arrow
+//         change arrow 
         rest_tail -> next = head;
-// mark first node next null because it is last node now
-     head -> next = NULL;
+        head -> next = NULL;
         
-//         NEW HEAD 
+//     New head
         return rest_head;
     }
 };
