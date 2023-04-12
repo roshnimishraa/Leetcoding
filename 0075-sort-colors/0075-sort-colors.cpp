089df4072fb766e0d629a8197e0af8dca8fb28bb
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-04-12 11:44:20 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (92.68%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -7,15 +7,19 @@ public:
         
         while(mid <= high)
         {
+//             IF THE ELEMENT IS 0
             if(nums[mid] == 0)
             {
                 swap(nums[mid],nums[low]);
                 low++;
                 mid++;
             }
+//             IF THE ELEMENT IS 1
             else if(nums[mid] ==1 ){
                 mid++;
             }
+            
+//             IF THE ELEMENT IS 2            
             else{
                 swap(nums[mid],nums[high]);
                 high--;
