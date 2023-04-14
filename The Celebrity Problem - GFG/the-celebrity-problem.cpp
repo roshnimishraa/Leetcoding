6794e072cee8bd8b3a72ce7f121b0f51d2709c04
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-04-14 23:41:02 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -10,77 +10,65 @@ using namespace std;
 class Solution 
 {
     private:
-bool knows(vector<vector<int> >& M,int a,int b,int n)
-{
-    
+    bool knows(vector<vector<int>> &M,int a,int b,int n){
+         
   if(M[a][b] == 1)
       return true;
          
   else
       return false;
-  
-}
+    }
     public:
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
-    stack<int> s;
+         stack<int> st;
         //step1: push all element in stack
         for(int i=0; i<n; i++) {
-            s.push(i);
+            st.push(i);
         }   
-        
-        //step2: get 2 elements and copare them
-        
-        while(s.size() > 1) {
-            
-            int a = s.top();
-            s.pop();
-            
-            int b = s.top();
-            s.pop();
+// step 2 : get 2 elements
+while(st.size() > 1){
+    int a = st.top();
+    st.pop();
+    int b = st.top();
+    st.pop();
 
-// if a knows b then discard a and push b
-            if(knows(M,a,b,n)){
-                s.push(b);
-            }
-// if b knows a then discard b and push a
-            else
-            {
-                s.push(a);
-            }
-        }
-        int ans = s.top();
-        //step3: single element in stack is potential celeb
-        //so verify it
-        
-        int zeroCount = 0;
- // 1. RowCheck: Celebrity knows NO ONE - all 0
- 
-        for(int i=0; i<n; i++) {
-            if(M[ans][i] == 0)
-                zeroCount++;
-        }
-        
-        // check all zeroes
-        if(zeroCount != n)
-            return -1;
-        
-        //column check
-// Everyone knows Celebrity: All 1 except diagonal(0)
-        int oneCount = 0;
-        
-        for(int i=0; i<n; i++) {
+// if a knows b: discard a 
+if(knows(M,a,b,n)){
+    st.push(b);
+}
+else{
+    st.push(a);
+}
+}
+
+// step 3: verity potential celebrity
+int ans = st.top();
+// RowCheck all 0
+int zeroCount =0;
+for(int i=0;i<n;i++){
+    if(M[ans][i] == 0)
+        zeroCount++;
+    
+}
+
+if(zeroCount !=n)
+    return -1;
+
+// Column check all 1 except diagonal
+int oneCount =0;
+    for(int i=0; i<n; i++) {
             if(M[i][ans] == 1)
                 oneCount++;
         }
-        
-        if(oneCount != n-1)
-            return -1;
-        
+// check all 1 except diagonal 
+if(oneCount != n-1)
+return -1;
+     
         return ans;
-        
     }
+    
 };
 
 //{ Driver Code Starts.
