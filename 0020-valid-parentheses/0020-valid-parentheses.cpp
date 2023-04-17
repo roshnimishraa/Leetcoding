030f6609ec11d70807d4d47141b4d841cb97e8d1
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-04-17 16:42:25 +0530

    Time: 4 ms (38.98%), Space: 6.4 MB (56.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,51 +1,47 @@
 class Solution {
-    bool matching(char top,char ch)
-    {
-return ((ch == ')' && top == '(') ||
-  (ch == '}' && top == '{')  ||
-   (ch == ']' && top == '[') 
-  );
-    }
+    private:
+    bool matches(char top,char ch){
+    
+        if(ch == ')' && top == '(' ||
+          ch == ']' && top == '[' ||
+          ch=='}'  && top == '{')
+            
+      return true;
+   
+else
+    return false;
+
+}
 public:
     bool isValid(string s) {
-        
         stack<char> st;
-        for(int i=0;i<s.length(); i++){
-          //         ith value 
-            char ch = s[i];
+//         step 1: push into stack 
+        for(int i=0;i<s.length();i++){
+            int ch = s[i];
             
-//    if opening bracket, stack push 
-//    if closing bracket, stack top check and pop 
-        
-//             For opening bracket
-    if(ch == '(' || ch == '{' || ch == '[' ){
-           st.push(ch);
-        
-    }
-            
-else{
-//     For Closing bracket
-    if(!st.empty())
+   if(ch=='(' || ch=='{' || ch=='[')
     {
-
-        char top = st.top();
+            st.push(ch);
+    }    
+            else{
+                if(!st.empty()){
+          char top = st.top();
     
        //  Comparing with stack top  match(top,current character)
-        if(matching(top,ch)){
+        if(matches(top,ch)){
             st.pop();
         }
         else{
             return false;
         }
     }
-//     closing bracket nhi mila or stack is empty
+        //     closing bracket nhi mila or stack is empty
     else{
         return false;
     }
 }
         }
-// after matching stack is empty
-        if(st.empty()){
+   if(st.empty()){
             return true;
         }
         else{
