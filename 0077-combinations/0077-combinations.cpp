8af4779918b5c8ed7f75c29191f1daa21c9fc323
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-04-19 11:49:17 +0530

    Time: 32 ms (42.12%), Space: 18 MB (32.00%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -2,25 +2,29 @@ class Solution {
     void helper(int index,int n,int k,vector<int> &output,vector<vector<int>> &ans)
     {
 if(k==0){
-ans.push_back(output);
-return;
- }
-   if(index >n){
- return;
-  }
-// include
-output.push_back(index); 
-helper(index+1,n,k-1,output,ans);
-output.pop_back();
-
-// exclude
-helper(index+1,n,k,output,ans);
+    ans.push_back(output);
+    return;
+}
+        
+ if(index > n)
+ {
+           return; 
+}
+        
+//     include
+        output.push_back(index);
+        helper(index+1,n,k-1,output,ans);
+        output.pop_back();
+        
+// exclude 
+        helper(index+1,n,k,output,ans);
     }
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
-        vector<int> output;
         int index=1;
+      vector<int> output;
+        
         helper(index,n,k,output,ans);
         return ans;
     }
