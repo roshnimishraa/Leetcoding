194d0478947459920c2fb6173ab1c72f34f22666
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-04-20 15:39:26 +0530

    Time: 6 ms (70.33%), Space: 16.9 MB (31.55%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+
+void helper(int index,int sumTillNow,vector<int>& arr,int target,
+           vector<int> &output,vector<vector<int>> &ans)
+{
+    int n = arr.size();
+    if(sumTillNow == target){
+        ans.push_back(output);
+    }
+    if(sumTillNow >= target){
+        return;
+    }
+    if(index >= n){
+        return;
+    }
+
+//     exclude
+// recursive call 
+    helper(index+1,sumTillNow,arr,target,output,ans);
+
+//     include
+    sumTillNow += arr[index];
+output.push_back(arr[index]);
+    helper(index,sumTillNow,arr,target,output,ans);
+    output.pop_back();
+}
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        
+        int sumTillNow =0;
+            int index=0;
+
+        helper(index,sumTillNow,candidates,target,output,ans);
+        return ans;
+    }
+};
