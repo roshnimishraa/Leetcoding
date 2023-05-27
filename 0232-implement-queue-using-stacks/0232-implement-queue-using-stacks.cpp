d65b3baae1135bb757d75dc3e95b5d57b9b1dcfc
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-05-27 11:20:33 +0530

    Time: 0 ms (100.00%), Space: 7 MB (30.52%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,54 +1,44 @@
-
-/*Algo For push 
-push(x)
-1. s1-> s2
-2. x->s1
-3. s2 -> s1
-
-pop()
-s1.pop()
-*/
-
-
 class MyQueue {
 public:
-    stack<int>s1;
+    stack<int> s1;
     stack<int> s2;
     MyQueue() {
         
     }
     
     void push(int x) {
-while(!s1.empty())
-{
-    s2.push(s1.top());
-    s1.pop();
-
-}
-
-s2.push(x);
-while(!s2.empty())
-{
-    s1.push(s2.top());
-    s2.pop();
-}
+        while(s1.empty() == false)
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s2.push(x);
+        while(s2.empty() == false){
+        s1.push(s2.top());
+            s2.pop();
+        }
+    }
     
-}
-    
-    int pop() 
-    {
-int ans = s1.top();
-s1.pop();
-return ans;        
-  }
+    int pop() {
+    int ans = s1.top();
+        s1.pop();
+        return ans;
+    }
     
     int peek() {
-       
-   return s1.top();
+        return s1.top();
     }
     
     bool empty() {
-     return s1.empty();
+        return s1.empty();
     }
 };
 
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
