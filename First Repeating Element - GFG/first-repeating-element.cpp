1556e6ce6f184d46c7023e19de062080e8fde274
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-07-25 16:11:56 +0530

    Added solution - LeetHub

--- First Repeating Element - GFG/first-repeating-element.cpp
+++ First Repeating Element - GFG/first-repeating-element.cpp
@@ -9,23 +9,20 @@ using namespace std;
 
 class Solution {
   public:
-   
+    // Function to return the position of the first repeating element.
     int firstRepeated(int arr[], int n) {
         unordered_map<int,int> umap;
-        
-    for(int i=0;i<n;i++){
-        umap[arr[i]]++;
-    }
-    
-    //logic
-    for(int i=0;i<n;i++){
-        int key = arr[i];
-        auto temp = umap.find(key);
-        if(temp->second > 1){
-            return i+1;
+        for(int i=0;i<n;i++){
+            umap[arr[i]]++;
         }
-    }
-    return -1;
+        
+        for(int i=0;i<n;i++){
+            int key = arr[i];
+    auto temp = umap.find(key);
+    if(temp->second > 1)
+    return i+1;
+        }
+        return -1;
     }
 };
 
