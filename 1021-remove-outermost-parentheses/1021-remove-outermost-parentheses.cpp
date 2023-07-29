3f02301bd3f2527d49c482190eaadcdc6dc2592f
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-07-29 19:50:28 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (39.28%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<int> st;
+        string ans;
+        
+        for(int i=0;i<s.length(); i++){
+            char ch = s[i];
+            if(ch == '(')
+            {
+                
+                if(st.size() > 0)
+                {
+                    ans += '(';
+                }
+                st.push(ch);
+            }
+            else{
+                //close bracket 
+                st.pop();
+                if(st.size() > 0){
+                ans += ')';
+                }
+            }
+        }
+        return ans;
+    }
+};
