1df59b6ae6860e0eed0153b7eecb5e518ce48458
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-07-30 11:45:43 +0530

    Time: 141 ms (98.67%), Space: 66.5 MB (49.89%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,53 +1,52 @@
 class Solution {
-    int possible(vector<int> &arr,int day,int m,int k){
-        int count=0;
-        int n=arr.size();
-        int noOfBouquet = 0;
-      //day = mid
+    int possible(vector<int>& arr,int mid,int m,int k){
+long long count = 0;
+        long long noOfB =0;
+long long n=arr.size();
         for(int i=0;i<n;i++){
-            if(arr[i] <= day){
-                count++;
-            }
-            else{
-               noOfBouquet += (count/k);
-                count = 0; 
-            }
+     if(arr[i] <=mid){
+  count++;
+         
+     }
+        else {
+         
+            noOfB += (count/k);
+               count =0;
         }
-        //No. of bouquets in any array can be formed 
-       noOfBouquet += (count/k);
-        return noOfBouquet >=m; 
+        }
+    
+        noOfB += (count/k);
+        return noOfB >=m;
     }
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-        //corner case m*k > n 
-        long long val=1LL*m*k;
-        if(val > n){
+  long long n=bloomDay.size();
+      long long ans = -1;
+    long long val = 1LL*m*k;
+        if(val > n) 
+        {
             return -1;
         }
-    //find min & max in an array 
-        int mini = INT_MAX;
+       int mini = INT_MAX;
         int maxi = INT_MIN;
+      
         for(int i=0;i<n;i++){
+            mini = min(bloomDay[i],mini);
+            maxi = max(bloomDay[i],maxi);
             
-                 mini = min(bloomDay[i],mini);
-                maxi = max(bloomDay[i],maxi);
-         
         }
-        
-//   apply binary search 
-        int low = mini,  high = maxi;
-        int ans =-1;
-    while(low<= high){
-        int mid= low + (high-low)/2;
-        if(possible(bloomDay,mid,m,k))
-        {
-            ans = mid;
-            high = mid-1;
-        }
-        else{
-            low = mid+1;
-        }
+        long long  low = mini;
+        long long  high =maxi;
+        while(low <= high ){
+            int mid = low + (high - low)/2;
+            
+            if(possible(bloomDay,mid,m,k)){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
         }
         return low;
     }
