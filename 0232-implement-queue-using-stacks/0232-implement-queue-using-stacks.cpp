dda4e4131ab0b750b8c755a48494513e262f266e
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-01 20:49:23 +0530

    Time: 3 ms (34.03%), Space: 6.9 MB (45.39%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -9,34 +9,34 @@ public:
     
     void push(int x) {
         input.push(x);
-        
+       
     }
     
     int pop() {
-        if(output.empty())
-        {
-        while(!input.empty())
-        {
-            output.push(input.top());
-        input.pop();
+       
+       
+      if(output.empty() ==true) {
+            while(input.empty() == false){
+               output.push(input.top());
+            input.pop();
+         
+            }
+               
         }
-        }
-        
-    int ans = output.top();
+         int ans =output.top();
         output.pop();
-        return ans;
+     return ans;   
     }
     
     int peek() {
-        if(output.empty())
-        {
-while(!input.empty()){
-    output.push(input.top());
-    input.pop();
-}
-        }
-        
-    return output.top();
+    
+       if(output.empty()){
+           while(!input.empty()){
+               output.push(input.top());
+               input.pop();
+           }
+       }
+        return output.top();
     }
     
     bool empty() {
