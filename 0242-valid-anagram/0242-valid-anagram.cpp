3606c0c71843975d3a096a1bf2a8cb2e45842386
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-03 19:30:26 +0530

    Time: 3 ms (98.35%), Space: 7.4 MB (12.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()){
-            return false;
+        unordered_map<char,int> umap;
+        
+    if(s.length() != t.length()) return false;
+        
+    for(int i=0;i<s.length();i++){
+        
+        umap[s[i]]++;
+        umap[t[i]]--;
+    }
+        for(auto it:umap){
+            if(it.second) return false;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
-        }
-        return false;
+        return true;
     }
 };
