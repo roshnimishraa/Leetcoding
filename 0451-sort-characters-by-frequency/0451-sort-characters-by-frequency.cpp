0cb4b9924e9cdfac1b560b9da1ebca5a1bd8d116
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-04 13:40:47 +0530

    Time: 8 ms (91.33%), Space: 8.5 MB (44.76%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<vector<char>> bucket(s.size()+1); // DONT FORGET +1 to size!!
-        unordered_map<char, int> m;
-        string ans;
+     unordered_map<char,int>umap;
+    string ans="";
         
-        // 1. map of character to its frequency        
-        for (const auto& e : s)        
-            m[e]++;        
-           
-		// 2. make the buckets
-        // idx represents frequency
-        // value at index represents characters with that frequency
-        for (const auto& e : m)        
-            bucket[e.second].push_back(e.first);
+        for(int i=0;i<s.length();i++){
+            umap[s[i]]++;
+            
+        }
         
-                
-        // 3. now reconstruct the string
-        for (int i = bucket.size() - 1; i > 0; i--)
-        {
-            // for each character in the bucket
-            for (int j = 0; j < bucket[i].size(); j++)
-            {
-                // append it i number of times
-                // i.e character 'e' at bucket 3 will be appended 3 times -> "...eee"
-                for (int count = i; count > 0; count--)
-                    ans += bucket[i][j];
+        priority_queue<pair<int,char>> pq;
+        
+        for(auto it:umap){
+            pq.push({it.second,it.first});
+            
+        }
+        
+        while(!pq.empty()){
+            int n=pq.top().first;
+            
+            while(n--){
+                ans+=pq.top().second;
             }
-        }          
-        
+            pq.pop();
+        }
         return ans;
     }
 };
