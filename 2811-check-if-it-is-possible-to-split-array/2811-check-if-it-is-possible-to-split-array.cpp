101e7264950e280f2c7da58322990000b2b404ff
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-06 12:14:05 +0530

    Time: 12 ms (66.67%), Space: 17.8 MB (66.67%) - LeetHub

---
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canSplitArray(vector<int>& nums, int m) {
+//         corner case 
+        if(nums.size() <= 2) return true;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]+nums[i-1] >= m){
+                return true;
+            }
+        }
+        return false;
+    }
+};
