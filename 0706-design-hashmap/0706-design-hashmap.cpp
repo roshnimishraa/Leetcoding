e8290786feb78bad5888f527d9e31de790977936
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-11 00:34:52 +0530

    Time: 110 ms (89.89%), Space: 52.2 MB (83.35%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,52 +1,52 @@
 class MyHashMap {
 public:
- vector<list<pair<int,int>>> m;
+    vector<list<pair<int,int>>> m;
     int size;
     MyHashMap() {
         size=100;
         m.resize(size);
     }
     int hash(int key){
-    return key%size;
+        return key % size;
     }
 //     search function 
-//     iterator to iterate through the list 
-   list<pair<int,int>> :: iterator search(int key){
-        int i=hash(key);
-// iterator to find key and value 
-   list<pair<int,int>> :: iterator it=m[i].begin();
+
+list<pair<int,int>> :: iterator search(int key){
+int i=hash(key);
+//  iterator to iterate through elements 
+list<pair<int,int>> :: iterator it = m[i].begin();
   
-       while(it != m[i].end()) // jb tk mai search krti rahungi
-       {
-           if(it->first == key) 
-               return it; 
-           it++;
-       }
-       return it;
+//     iterate till the end 
+    while(it != m[i].end()) {
+        if(it->first == key)
+            return it;
+            it++;
+      
+    }
+    return it;
     }
     void put(int key, int value) {
-        int i=hash(key);
-//    if key already exists 
+     int i = hash(key);
+        // if already exist 
         remove(key);
-        m[i].push_back({key,value});
+    m[i].push_back({key,value});
+        
     }
     
     int get(int key) {
-       int i=hash(key);
+        int i=hash(key);
     list<pair<int,int>> :: iterator it = search(key);
-        
-//   if element  does not exist 
         if(it == m[i].end()) return -1;
         else return it->second;
     }
     
     void remove(int key) {
-     int i = hash(key);
-list<pair<int,int>> :: iterator it=search(key);
-        
-    // if exists then remove in doubly linked list use erase 
- if(it != m[i].end())   m[i].erase(search(key));
-        
+        int i=hash(key);
+    list<pair<int,int>> :: iterator it = search(key);
+//         element exists
+        if(it != m[i].end()){
+            m[i].erase(search(key));
+        }
     }
 };
 
