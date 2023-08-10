1a8b97180725435eeeb80043670131a052b951ad
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-10 22:50:38 +0530

    Time: 92 ms (76.18%), Space: 40.9 MB (79.44%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,22 +1,44 @@
 class MyHashSet {
 public:
+      vector<list<int>> m;
     int size;
-    vector<int> m;
+//     CHAINING METHOD TO AVOID COLLISON 
     MyHashSet() {
-    size = 1e6+1;
+    size = 100;
         m.resize(size);
+        
+    }
+//   to insert -> k % m
+    int hash(int key){
+        return key % size;
     }
     
+// iterator to iterate through list 
+list<int> :: iterator search(int key){
+    int i=hash(key);  
+    return find(m[i].begin(),m[i].end(),key);
+}
     void add(int key) {
-    m[key]=1;
+//      avoid duplicacy
+        if(contains(key)) return;
+// otherwise
+        int i=hash(key); //find index
+        m[i].push_back(key);
     }
     
     void remove(int key) {
-        m[key] =0;
+        if(!contains(key)) return;
+        int i=hash(key);
+//      erase function for doubly linked list
+        m[i].erase(search(key));
     }
     
     bool contains(int key) {
-        return m[key];
+     int i=hash(key);       
+    //if(search(key) == m.end()) element is not present 
+        if(search(key) != m[i].end())  // element is present
+            return true;
+        else return false;
     }
 };
 
