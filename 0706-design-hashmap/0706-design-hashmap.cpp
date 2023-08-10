ef70b09d566a00dcd17b324bffc1f396726c40ac
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-11 00:18:53 +0530

    Time: 104 ms (93.27%), Space: 52.2 MB (87.15%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,30 +1,52 @@
 class MyHashMap {
 public:
-    vector<int> m;
+ vector<list<pair<int,int>>> m;
     int size;
     MyHashMap() {
-        size=1e6+1;
-      m.resize(size);
-        fill(m.begin(),m.end(),-1);
+        size=100;
+        m.resize(size);
     }
-    
+    int hash(int key){
+    return key%size;
+    }
+//     search function 
+//     iterator to iterate through the list 
+   list<pair<int,int>> :: iterator search(int key){
+        int i=hash(key);
+// iterator to find key and value 
+   list<pair<int,int>> :: iterator it=m[i].begin();
+  
+       while(it != m[i].end()) // jb tk mai search krti rahungi
+       {
+           if(it->first == key) 
+               return it; 
+           it++;
+       }
+       return it;
+    }
     void put(int key, int value) {
-m[key] = value;
+        int i=hash(key);
+//    if key already exists 
+        remove(key);
+        m[i].push_back({key,value});
     }
     
     int get(int key) {
-        return m[key];
+       int i=hash(key);
+    list<pair<int,int>> :: iterator it = search(key);
+        
+//   if element  does not exist 
+        if(it == m[i].end()) return -1;
+        else return it->second;
     }
     
     void remove(int key) {
-      m[key]=-1;
+     int i = hash(key);
+list<pair<int,int>> :: iterator it=search(key);
+        
+    // if exists then remove in doubly linked list use erase 
+ if(it != m[i].end())   m[i].erase(search(key));
+        
     }
 };
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
