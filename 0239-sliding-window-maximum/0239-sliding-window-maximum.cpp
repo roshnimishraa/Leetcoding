9ad37f37f60eaa729430a124f43f1dcb2cb4aa61
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-16 21:53:10 +0530

    Time: 207 ms (86.58%), Space: 134.6 MB (66.10%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,25 +4,26 @@ public:
         int n = nums.size();
         deque<int> dq;
         vector<int> ans;
+          if(n == 0)
+            return {}; 
+        
     for(int i=0;i<n;i++){
-   
-/* 1. m When new element comes nums[i], make space for it (Window Size can not be greater than K) */
- 
+    //1. make space for nums[i] till window size(i-k)
+   // jo element push kr rhe h window size me agar uska prev usse small hai 
+// deq.front se pop kr do
     while(!dq.empty()  && dq.front() <= i-k){
         
         dq.pop_front();
     }
         
-/*  2. Now, when nums[i] comes there is no need to keep small elements in  that window pop */
-        
+//2. Jo next element agega vo prev wale se big ele to prev ko pop kr dena h
     while(!dq.empty() && nums[i] > nums[dq.back()]){
         dq.pop_back();
     }
-        
-//3 now push i in dequeu -> for nums[i]
+//3 push i
   dq.push_back(i);
         
-//4. if i>= k-1 then deq.front() is the answer for particular k size window  
+//4. 
         if(i>= k-1) {
             ans.push_back(nums[dq.front()]);
         }
