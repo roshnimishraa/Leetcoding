e10838a6955a305dd33e8d7aac5116076282406d
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-16 22:00:58 +0530

    Time: 219 ms (81.68%), Space: 134.6 MB (66.10%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        deque<int> dq;
+        int n=nums.size();
         vector<int> ans;
-          if(n == 0)
-            return {}; 
-        
-    for(int i=0;i<n;i++){
-    //1. make space for nums[i] till window size(i-k)
-   // jo element push kr rhe h window size me agar uska prev usse small hai 
-// deq.front se pop kr do
-    while(!dq.empty()  && dq.front() <= i-k){
-        
+        deque<int> dq;
+    //chhota ele remove kro 
+for(int i=0;i<n;i++){
+    while(!dq.empty() && dq.front() <= i-k ){
         dq.pop_front();
     }
-        
-//2. Jo next element agega vo prev wale se big ele to prev ko pop kr dena h
-    while(!dq.empty() && nums[i] > nums[dq.back()]){
-        dq.pop_back();
+    
+while(!dq.empty() && nums[i] > nums[dq.back()]){
+dq.pop_back();
+}
+    dq.push_back(i);
+    if(i>= k-1){
+    ans.push_back(nums[dq.front()]);
     }
-//3 push i
-  dq.push_back(i);
-        
-//4. 
-        if(i>= k-1) {
-            ans.push_back(nums[dq.front()]);
-        }
-    }
+}
         return ans;
     }
 };
