c22296eae40d254d6d89166dddd2f98ae0b05987
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-23 20:00:07 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (46.07%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,52 +1,52 @@
 class Solution {
 public:
+    typedef pair<int, char> P;
+    
     string reorganizeString(string s) {
-          string ans="";
-        unordered_map<char,int> umap;
-     priority_queue<pair<int,char>> pq;
+        int n = s.length();
         
-    // push char into umap
-        for(auto ch : s){
-            umap[ch]++;
+        vector<int> count(26, 0);
+        for(char &ch : s) {
+            count[ch-'a']++;
+
+            if(count[ch-'a'] > (n+1)/2)
+                return "";
         }
         
-    for(auto itr : umap){
-        pq.push({itr.second,itr.first});
-    }
-        
-    //fetching the top most element 
-    while(pq.size() > 1){
-        auto top1 = pq.top();
-        pq.pop();
-        
-    auto top2 = pq.top();
-        pq.pop();
-        
-    //store char in ans 
-        ans += top1.second;
-        ans += top2.second;
+        priority_queue<P, vector<P>> pq;
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+            if(count[ch-'a'] > 0) {
+                pq.push({count[ch-'a'], ch});
 
-// after inserting char into pq decrease size by 1 
-        top1.first -= 1;
-        top2.first -= 1;
+            }
+         }
         
-//  if frequency of char > 0 then again push in pq
-    if(top1.first > 0){
-        pq.push(top1);
-    }
+        string result = "";
         
-    if(top2.first > 0){
-        pq.push(top2);
-    }
-    }
-        
-    if(!pq.empty()) {
-        if(pq.top().first > 1)  return "";
-        
-        else 
-            ans += pq.top().second;
+        while(pq.size() >= 2) {
+            auto P1 = pq.top(); 
+            pq.pop();
             
-    }
-        return ans;
+            auto P2 = pq.top();
+            pq.pop();
+            
+            result.push_back(P1.second);
+            result.push_back(P2.second);
+            
+            P1.first--;
+            P2.first--;
+            
+            if(P1.first > 0)
+                pq.push(P1);
+            
+            if(P2.first > 0)
+                pq.push(P2);
+        }
+        
+        if(!pq.empty())
+            result.push_back(pq.top().second);
+        
+        return result;
     }
 };
+
