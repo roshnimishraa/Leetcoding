c494fa296c1f32d33296e54fcd76fa806f67f915
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-10-17 23:35:04 +0530

    Time: 62 ms (35.11%), Space: 44.1 MB (20.51%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1361-validate-binary-tree-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1361-validate-binary-tree-nodes.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1361-validate-binary-tree-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1361-validate-binary-tree-nodes.cpp
@@ -1,74 +1,71 @@
 class Solution {
 public:
-    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) { 
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild,
+    vector<int>& rightChild) 
+    {
+        vector<vector<int>> graph(n);
+        int edge =0;
         
-        // CREATE A GRAPH
-       
-        vector<vector<int>> graph(n); 
-        
-        //NUMBER OF EDGES IN GRAPH
-        int edge = 0; 
-        
-        for(int i = 0; i < n; i++) {   
-            if(leftChild[i] != -1) {
-                graph[i].push_back(leftChild[i]);   
-                ++edge;
-            } 
-            if(rightChild[i] != -1) { 
-                graph[i].push_back(rightChild[i]); 
-                ++edge;
-            } 
+    for(int i=0;i<n;i++)
+    {
+        if(leftChild[i] != -1)
+        {
+            graph[i].push_back(leftChild[i]);
+            ++edge;
         }
+        if(rightChild[i] != -1)
+        {
+            graph[i].push_back(rightChild[i]);
+            ++edge;
+        }
+    }
         
-        //CHECK FOR SELF LOOPS AND CYCLES
-        queue<int> q; 
-        vector<bool> visited(n, false); 
-        vector<int> indegree(n, 0); 
-        int ctr = 0; 
+    //check for self loop and cycle 
         
-        for(int i = 0; i < graph.size(); i++) { 
-            for(int j = 0; j < graph[i].size(); j++) { 
-                indegree[graph[i][j]]++;
-            }
-        } 
+    queue<int> q;
+vector<bool> visited(n,false);
+        vector<int> indegree(n,0);
+        int counter =0;
         
-        for(int i = 0; i < indegree.size(); i++) {  
-            
-            //NODE SHOULD HAVE EITHER ZERO OR ONE AS INDEGREE 
-            
-            if(indegree[i] > 1) { 
-                return false;
-            }
-            if(indegree[i] == 0) { 
-                q.push(i); 
-                ctr++;
-            }
-        } 
+
+for(int i=0;i<graph.size();i++){
+    for(int j=0;j < graph[i].size();j++){
+        indegree[graph[i][j]]++;
+    }
+}
+for(int i=0;i<indegree.size();i++)
+{
+    if(indegree[i]>1){
+        return false;
+    }
+    if(indegree[i] == 0){
+        q.push(i);
+        counter++;
+    }
+}
         
-        while(!q.empty()) { 
-            int front = q.front(); 
-            visited[front] = true; 
-            q.pop(); 
-            
-            for(int i : graph[front]) { 
-                
-                indegree[i]--;
-                
-                if(!visited[i] && indegree[i] == 0) { 
-                    q.push(i); 
-                    ctr++;
-                }
-            }
-        } 
-        
-        //IF CYCLES ARE PRESENT OR EDGES ARE NOT EQUAL TO N-1 RETURN FALSE 
-        
-        if(ctr != n || edge != n-1) { 
-            return false;
-        }  
-        
-        //ELSE RETURN TRUE 
-        
+while(!q.empty())
+{
+    int node = q.front();
+    visited[node] = true;
+    q.pop();
+    
+    
+for(auto i : graph[node])
+{
+    indegree[i]--;
+    if(!visited[i] && indegree[i] == 0)
+    {
+        q.push(i);
+        counter++;
+    }
+}
+}
+ //IF CYCLES ARE PRESENT OR EDGES ARE NOT EQUAL TO N-1 RETURN FALSE 
+ if(counter != n || edge != n-1)
+ {
+     return false;
+ }
         return true;
     }
 };
