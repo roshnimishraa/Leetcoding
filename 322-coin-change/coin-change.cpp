b11e9e3c86617c2f45e910a474a6004f5397f6f3
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-02-14 19:57:33 +0530

    Time: 45 ms (68.26%) | Memory: 46.8 MB (22.95%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,30 +1,30 @@
 class Solution {
-    int helper(int index,int target,vector<int> &coins,
-    vector<vector<int>> &dp)
-    {
-if(index==0) {
-if(target % coins[0] == 0) 
-return target/coins[0];
-else return 1e9;
-}
-if(dp[index][target]!=-1) 
-return dp[index][target];
-
-int notTake = 0+helper(index-1,target,coins,dp);
-int take = INT_MAX;
-if(coins[index] <= target)
-{
-    take = 1+helper(index,target-coins[index],coins,dp);
-}
-return dp[index][target] = min(take,notTake);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n =coins.size();
-vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-
-int ans=helper(n-1,amount,coins,dp);
-if(ans >= 1e9){
+        int n= coins.size();
+vector<vector<int>> dp(n,vector<int>(amount+1,0));
+for(int target=0;target<=amount;target++)
+{
+if(target % coins[0]==0)
+dp[0][target] = target/coins[0];
+else 
+dp[0][target]= 1e9;
+}
+for(int ind=1;ind<n;ind++)
+{
+for(int target=0;target<=amount;target++)
+{
+int notTake = 0+dp[ind-1][target];
+int take =INT_MAX;
+if(coins[ind]<=target)
+{
+take = 1 + dp[ind][target-coins[ind]];
+}
+dp[ind][target] = min(take,notTake);
+}
+}
+int ans = dp[n-1][amount];
+if(ans>=1e9){
     return -1;
 }
 return ans;
