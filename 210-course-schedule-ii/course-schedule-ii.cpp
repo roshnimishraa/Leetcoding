4076670354089fff238330d52b08c98a3002923e
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-02-15 23:53:10 +0530

    Time: 15 ms (78.06%) | Memory: 17.7 MB (20.44%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> findOrder(int N, vector<vector<int>>& prerequisites)
+    {
+        int n = prerequisites.size();
+   vector<vector<int>> adj(N);
+for(auto it:prerequisites)
+{
+adj[it[1]].push_back(it[0]);
+}
+// kahn's algo
+vector<int> indegree(N,0);
+for(int i=0;i<N;i++)
+{
+for(auto it:adj[i])
+{
+    indegree[it]++;
+}
+}
+queue<int> q;
+for(int i=0;i<N;i++)
+{
+    if(indegree[i]==0)
+    {
+q.push(i);
+    }
+}
+vector<int> ans;
+while(!q.empty()){
+    auto node = q.front();
+    q.pop();
+ans.push_back(node);
+for(auto it:adj[node])
+{
+indegree[it]--;
+if(indegree[it] == 0)
+{
+    q.push(it);
+}
+}
+}
+if(ans.size() == N) return ans;
+else return {};
+    }
+};
