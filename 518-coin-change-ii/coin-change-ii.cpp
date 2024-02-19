13a35dd49b592da242a11ad459ab4fe0bb107e1f
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-02-20 01:14:20 +0530

    Time: 11 ms (63.69%) | Memory: 19.9 MB (24.97%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    private:
+int helper(int index,int target,vector<int> &coins,vector<vector<int>> &dp)
+{
+// base case 
+if(index == 0){
+    if(target % coins[0] == 0) return 1;
+    else return 0;
+}
+if(dp[index][target]!=-1) return dp[index][target];
+
+int notTake = helper(index-1,target,coins,dp);
+int take=0;
+if(coins[index] <= target) 
+{
+take = helper(index,target-coins[index],coins,dp);
+}
+return dp[index][target]=(take+notTake);
+}
+public:
+    int change(int amount, vector<int>& coins) {
+int n = coins.size();
+vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+return helper(n-1, amount, coins,dp);    
+    }
+};
