d36565c53c34b42d7f9f7235dad2b93accd4d51b
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-03-03 12:37:27 +0530

    Time: 245 ms (35.85%) | Memory: 294.7 MB (6.00%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,21 @@
 class Solution {
-    private:
-int helper(int ind,int prev,vector<int> &nums,vector<vector<int>> &dp)
-{
-    int n = nums.size();
-if(ind == n) {
-return 0; //run out of elements
-}
-if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-// -1 as prev ind can't be stored in table 
-// therefore coordinate change prevInd+1
-int notTake = 0+helper(ind+1,prev,nums,dp);
-int take=0;
-if(prev==-1 || nums[ind]>nums[prev])
-{
-    take = 1+helper(ind+1,ind,nums,dp);
-}
-return dp[ind][prev+1]=max(take,notTake);
-}
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-    return helper(0,-1,nums,dp);
-    
+    int n=nums.size();
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+for(int ind=n-1;ind>=0;ind--)
+{
+for(int prevInd=ind-1;prevInd>=-1;prevInd--)
+{
+int take=0,notTake=0;
+notTake = 0+dp[ind+1][prevInd+1];
+if(prevInd == -1 || nums[ind] > nums[prevInd])
+{
+take = 1 + dp[ind+1][ind+1];
+}
+dp[ind][prevInd+1] = max(take,notTake);
+}
+}
+return dp[0][0]; //dp[0][-1+1]
     }
 };
