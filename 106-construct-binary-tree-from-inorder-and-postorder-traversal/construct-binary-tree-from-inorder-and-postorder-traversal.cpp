50e7606de21796ca988202838c0d606f6c185f14
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-03-07 17:39:26 +0530

    Time: 3 ms (99.08%) | Memory: 25.9 MB (67.77%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -10,21 +10,27 @@
  * };
  */
 class Solution {
+    private:
+    TreeNode* construct(vector<int> &inorder,int instart,int inend,vector<int> &postorder,int poststart,int postend,map<int,int> &mp)
+    {
+if(poststart > postend || instart>inend) 
+return NULL;
+
+TreeNode* root = new TreeNode(postorder[postend]);
+int inRoot = mp[postorder[postend]];
+int numsLeft = inRoot - instart;
+root->left = construct(inorder,instart,inRoot-1,postorder,poststart,poststart+numsLeft-1,mp);
+root->right = construct(inorder,inRoot+1,inend,postorder,poststart+numsLeft,postend-1,mp);
+return root;
+
+    }
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        map<int,int> mp;
-        for(int i=0;i<inorder.size();i++){
-            mp[inorder[i]]=i;
-        }
-        return construct(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1,mp);
-    }
-    TreeNode* construct(vector<int> &inorder,int instart, int inend,vector<int> &postorder, int poststart,int postend,  map<int,int> &mp){
-        if(poststart>postend || instart>inend) return NULL;
-        TreeNode* root=new TreeNode(postorder[postend]);
-        int inRoot=mp[postorder[postend]];
-        int numsleft=inRoot-instart;
-        root->left=construct(inorder,instart,inRoot-1,postorder,poststart,poststart+numsleft-1,mp);
-        root->right=construct(inorder,inRoot+1,inend,postorder,poststart+numsleft,postend-1,mp);
-        return root;
+   map<int,int> mp;
+   for(int i=0;i<inorder.size();i++)
+   {
+       mp[inorder[i]]=i;
+   } 
+   return construct(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1,mp);
     }
 };
