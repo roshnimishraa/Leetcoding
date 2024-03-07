a19b593b6cf1686ab4b1d57a7bae75b2407712e5
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-03-07 18:47:06 +0530

    Time: 8 ms (50.67%) | Memory: 18.1 MB (67.78%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -12,40 +12,34 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == nullptr) return 0;
-        long long ans = 0;
+        if(root == NULL) return 0;
+    long long ans=0;
 // {node,index}
-queue<pair<TreeNode*,long long>> q;
+queue<pair<TreeNode*,int>> q;
 q.push({root,0});
 while(!q.empty())
 {
-long long size = q.size();
-// choose min index : to make index starting from zero
-// to reduce overflow condition 
+    long long size = q.size();
+    // minimum index
 long long mini = q.front().second;
-// width = lastIndex-firstIndex + 1
 long long first,last;
 for(int i=0;i<size;i++)
 {
-    // to make idx start 1 at each idx 
-    // for left child 2*i+1 and right child
 long long curr_id = q.front().second-mini;
 TreeNode* node = q.front().first;
 q.pop();
-
-if(i == 0) {
+if(i==0)
+{
     first = curr_id;
 }
-if(i == size-1){
+if(i==size-1)
+{
     last = curr_id;
 }
-if(node->left != NULL)
-{
-    q.push({node->left,2*curr_id+1});
-}
-if(node->right != NULL){
-    q.push({node->right,2*curr_id+2});
-}
+if(node->left!=NULL) 
+q.push({node->left,2*curr_id+1});
+if(node->right!=NULL)
+q.push({node->right,2*curr_id+2});
 }
 ans = max(ans,last-first+1);
 }
