ca625cdd96debb436b6ed14b7749717549bb0b29
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-03-18 10:32:18 +0530

    Time: 46 ms (22.98%) | Memory: 17.2 MB (34.76%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,47 +8,23 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL)
+        return NULL;
+ListNode* t1 = headA;
+ListNode* t2 = headB;
+while(t1!=t2)
 {
-if(headA==NULL || headB==NULL)
- return NULL;
-
-        ListNode* t1 = headA;
-        ListNode* t2 =headB;
-        int n1=0,n2=0;
-// compute length of LL1 
-while(t1!=NULL)
-{
-    n1++;
     t1 = t1->next;
-}
-// compute length of LL2
-while(t2!=NULL)
-{
-    n2++;
     t2 = t2->next;
-}
-if(n2>n1)
-{
-   while(n2>n1)
-   {
-    headB = headB->next;
-    n2--;
-   }
-}
-else if(n1>n2){
-    while(n1>n2)
-    {
-        headA = headA->next;
-        n1--;
+    if(t1 == t2) {
+        return t1;
     }
+if(t1 == NULL)
+t1 = headB;
+if(t2 == NULL)
+t2 = headA;
 }
-while(headA!=NULL && headB!=NULL)
-{
-    if(headA==headB) return headA;
-    headA = headA->next;
-    headB = headB->next;
-}
-return NULL;
+return t1;
     }
 };
