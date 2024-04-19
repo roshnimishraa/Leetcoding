6d901250d353021035a5385061c287e2c3124585
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-04-19 18:38:56 +0530

    Time: 146 ms (66.12%) | Memory: 66.8 MB (75.81%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-       priority_queue<pair<int,pair<int,int>>> pq;
-        vector<vector<int>> res;
-        
-        for(int i=0;i<points.size();i++){
-            pq.push({points[i][0]*points[i][0] + 
-            points[i][1]*points[i][1],{points[i][0],points[i][1]}});
-            
-            if(pq.size()>k){
-                pq.pop();
-            }
-        }
-
-        while(!(pq.empty())){
-           pair<int,int> p=pq.top().second;
-            res.push_back({p.first,p.second});
-            pq.pop();
-        }
-        return res;
+        priority_queue<pair<int,pair<int,int>>> pq;
+    vector<vector<int>> res;
+for(int i=0;i<points.size();i++)
+{
+    int x = points[i][0];
+    int y = points[i][1];
+pq.push({x*x + y*y, {x,y}});
+ 
+ if(pq.size() > k)
+ {
+    pq.pop();
+ }
+}
+while(!pq.empty())
+{
+    pair<int,int> p = pq.top().second;
+    res.push_back({p.first,p.second});
+    pq.pop();
+}
+return res;
     }
 };
