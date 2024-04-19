fa8fcf5bcaa210cbc76a42d3e8b1fa808b0345d8
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-04-19 18:35:42 +0530

    Time: 121 ms (92.82%) | Memory: 66.7 MB (77.82%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+       priority_queue<pair<int,pair<int,int>>> pq;
+        vector<vector<int>> res;
+        
+        for(int i=0;i<points.size();i++){
+            pq.push({points[i][0]*points[i][0] + 
+            points[i][1]*points[i][1],{points[i][0],points[i][1]}});
+            
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+
+        while(!(pq.empty())){
+           pair<int,int> p=pq.top().second;
+            res.push_back({p.first,p.second});
+            pq.pop();
+        }
+        return res;
+    }
+};
