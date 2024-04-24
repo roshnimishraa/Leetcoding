39f92027d7c44cb66f9bb7e6180417015f43e0ac
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-04-24 19:39:29 +0530

    Time: 28 ms (59.43%) | Memory: 24.9 MB (51.74%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
- vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
-    int n = strs.size();
-//sorted string->same string which belong to sorted string 
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+// {sorted string, vector of string which belongs to same family}
 unordered_map<string,vector<string>> mp;
-
+  vector<vector<string>> ans;
 for(int i=0;i<n;i++)
 {
     string temp = strs[i];
-sort(temp.begin(),temp.end());
+    sort(temp.begin(),temp.end());
+// map me uss string ke corresponding jo vector h use store krenge
 mp[temp].push_back(strs[i]);
-} 
+}
+// push ans in map 
 for(auto it:mp)
 {
-ans.push_back(it.second);
+    ans.push_back(it.second);
 }
 return ans;
-}
+    }
 };
