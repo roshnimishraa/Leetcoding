37070200a9e9bf9826c51f7e905f704fd0e748e7
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-05-10 20:44:46 +0530

    Time: 59 ms (14.50%) | Memory: 47.3 MB (22.92%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,30 +1,28 @@
 class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {
-        int n= coins.size();
-vector<vector<int>> dp(n,vector<int>(amount+1,0));
-for(int target=0;target<=amount;target++)
-{
-if(target % coins[0]==0)
-dp[0][target] = target/coins[0];
-else 
-dp[0][target]= 1e9;
-}
-for(int ind=1;ind<n;ind++)
-{
-for(int target=0;target<=amount;target++)
-{
-int notTake = 0+dp[ind-1][target];
-int take =INT_MAX;
+    int solve(int ind,vector<int> &coins,int target,vector<vector<int>> &dp)
+    {
+    if(ind==0)
+    {
+        if((target % coins[ind]) == 0)
+        return target/coins[ind];
+        else 
+        return 1e9;
+    }
+if(dp[ind][target]!=-1) return dp[ind][target];
+    int notTake = 0+solve(ind-1,coins,target,dp);
+    int take = INT_MAX;
 if(coins[ind]<=target)
 {
-take = 1 + dp[ind][target-coins[ind]];
+    take = 1 + solve(ind,coins,target-coins[ind],dp);
 }
-dp[ind][target] = min(take,notTake);
-}
-}
-int ans = dp[n-1][amount];
-if(ans>=1e9){
+return dp[ind][target]=min(take,notTake);
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+   int ans=solve(coins.size()-1,coins,amount,dp);
+if(ans >= 1e9){
     return -1;
 }
 return ans;
