4486a5587ca49cf1774d8961a2dc8e36ccd8d40b
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-05-12 21:28:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (61.47%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,34 +1,29 @@
 class Solution {
-    int solve(int index,string &s,vector<int> &dp)
-    {
-    int n = s.length();
-if(dp[index]!=-1) return dp[index];
-    // base case => valid split 
-    if(index == n) {
-        return 1;
-    }
-    if(s[index] == '0')
-    {
-        return 0;
-    }
-    
-// since only 2-digit mapping is given therefore 
-// partition can be done atmost 2 places 
-int op1 = solve(index+1,s,dp);
-int op2=0;
-if(index+1 < n)
-{
-if(s[index]=='1'||(s[index]=='2' && s[index+1]<='6'))
-{
- op2 = solve(index+2,s,dp);
-}
-}
-return dp[index]=op1+op2;
-    }
 public:
     int numDecodings(string s) {
         int n=s.length();
-        vector<int> dp(n+1,-1);
-       return solve(0,s,dp); 
+        vector<int> dp(n+1,0);
+    dp[n] = 1; //if reached till last index that means 
+            //1 valid solution is found
+for(int i=n-1;i>=0;i--)
+{
+ if(s[i]=='0') 
+ {
+  dp[i] = 0;
+ }
+ else{
+int op1= dp[i+1];
+int op2=0;
+if(i+1<n)
+{
+if(s[i]=='1'||(s[i]=='2' && s[i+1]<='6'))
+{
+ op2= dp[i+2];
+}
+}
+dp[i] = op1+op2;
+ }
+} 
+return dp[0];
     }
 };
