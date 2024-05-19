c0cdf4f47fd53c58337285e6c64da683d1949b15
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-05-19 22:19:06 +0530

    Time: 2 ms (59.43%) | Memory: 10.1 MB (60.00%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,27 +1,33 @@
 class Solution {
-public:
-int helper(vector<vector<int>> &grid,int i,int j,vector<vector<int>> &dp)
-{
-        int m = grid.size();
-    int n = grid[0].size();
-//robot can move to 0 cell and 1 is obstacle
-if(i<0 || i>= m || j<0 || j>=n || grid[i][j]==1)
-{
-return 0;
-}
-if(dp[i][j] != -1) return dp[i][j];
-if(i == m-1 && j==n-1) {
-    return 1;
-}
-int left = helper(grid,i,j+1,dp);
-int down = helper(grid,i+1,j,dp); 
-return dp[i][j] = left+down;
-}
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) 
+    int solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>> &dp)
     {
-    int m = grid.size();
-    int n = grid[0].size();
-vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-return helper(grid,0,0,dp);
+        //base case
+        if(i>=0 && j>=0 && grid[i][j]==1)
+        {
+            return 0;
+        }
+    if(i<0 || j<0)
+    {
+return 0;
+    } 
+    
+    if(i==0 && j==0){
+   return 1;
+    }
+ if(dp[i][j]!=-1)
+ {
+   return dp[i][j];
+ }
+
+    int down = solve(i-1,j,grid,dp);
+    int up = solve(i,j-1,grid,dp);
+    return dp[i][j]=down+up;
+    }
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+vector<vector<int>> dp(m,vector<int> (n,-1));
+        return solve(m-1,n-1,obstacleGrid,dp);
     }
 };
