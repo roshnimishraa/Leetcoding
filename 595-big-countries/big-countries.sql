2f6711b19df1ac559c68c0d8e87f59b6845f9587
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-07-22 19:25:01 +0530

    Time: 257 ms (74.42%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,8 +1,2 @@
--- using UNION
-select area, population, name
-from world
-where area >= 3000000
-union 
-select area,population, name
-from world
-where population >= 25000000
+select name, population, area from world
+where (area>= 3000000 or population >= 25000000)
