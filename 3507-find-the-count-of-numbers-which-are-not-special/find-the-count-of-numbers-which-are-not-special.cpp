6a8249d5786ea500aa7ececad614a645f2e6601f
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-07-28 11:34:57 +0530

    Time: 67 ms (84.62%) | Memory: 8.1 MB (100.00%) - LeetSync

---
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    private: 
+    bool isPrime(int x)
+    {
+      if(x == 2) return true;
+      if(x % 2 == 0) return false;
+      for(int i=3; i<=sqrt(x); i+=2){
+        if(x % i == 0) return false;
+      }
+      return true;
+    }
+public:
+    int nonSpecialCount(int l, int r) {
+        int specialNumbers = 0;
+        int total = r-l+1;
+    for(int i=2;i<= sqrt(r); i++){
+        if(i*i < l){ 
+            continue;
+        }
+        if(isPrime(i) == true){
+            specialNumbers++;
+        }
+    }
+    return total - specialNumbers;
+    }
+};
