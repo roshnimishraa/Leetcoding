239c56a70863e1222b8875b1140997737b5e53ee
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-07-28 10:49:49 +0530

    Time: 11 ms (16.67%) | Memory: 28.1 MB (16.67%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+       int sum1 = 0, sum2=0;
+       for(auto val: nums)
+       {
+        if(val < 10){
+            // alice 
+            sum1+= val;
+        }
+        else{
+            sum2+= val;
+        }
+       } 
+       return sum1 != sum2;
+    }
+};
