074c6f8cfe18075336a7315465f6551e714c43d5
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-19 12:52:26 +0530

    Time: 93 ms (66.10%) | Memory: 57.7 MB (87.88%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,29 +1,10 @@
 class Solution {
-    private:
-    int helper(int index, int buy, vector<int> &prices, int fee, vector<vector<int>> &dp)
-    {
-        int n = prices.size();
-        if(index==n){
-            return 0;
-        }
-        if(dp[index][buy] != -1)
-        {
-            return dp[index][buy];
-        }
-    int profit;
-    if(buy==1){
-    profit = max(-prices[index] + helper(index+1, 0, prices, fee, dp), 0 + helper(index+1, 1, prices, fee, dp));
-    }
-    if(buy == 0){
-        profit = max(prices[index] - fee + helper(index+1, 1, prices, fee, dp), 0 + helper(index+1, 0, prices, fee,dp));
-    }
-    return dp[index][buy] = profit;
-    }
+  
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-    vector<vector<int>> dp(n+1,vector<int> (2,0));
-  dp[n][0] = dp[n][1] = 0;
+vector<int> ahead(2,0); //index+1
+vector<int> curr(2,0);
   int profit;
   for(int index=n-1;index>=0;index--)
   {
@@ -31,15 +12,16 @@ for(int buy=0;buy<=1;buy++)
 {
     if(buy==1)
 {
-    profit = max(-prices[index] + dp[index+1][0],
-     0 + dp[index+1][1]);
+    profit = max(-prices[index] + ahead[0],
+     0 + ahead[1]);
 }
 if(buy == 0){
-    profit = max(prices[index]-fee + dp[index+1][1] , 0 + dp[index+1][0]);
+    profit = max(prices[index]-fee + ahead[1] , 0 + ahead[0]);
 }
-dp[index][buy] = profit;
+curr[buy] = profit;
 }
+ahead = curr;
   }
-  return dp[0][1];
+  return ahead[1];
     }
 };
