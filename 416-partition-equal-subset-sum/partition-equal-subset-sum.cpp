2cc8d2b628dd04ad4d5941d4fe91f0a6af51879f
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-19 14:44:02 +0530

    Time: 145 ms (57.58%) | Memory: 98.9 MB (48.24%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,29 +1,42 @@
 class Solution {
+    private:
+    bool helper(int index,int target,vector<int> &nums, vector<vector<int>> &dp)
+    {
+        if(target == 0)
+        {
+return true;
+        }
+        if(index==0)
+        {
+            return (nums[0]==target);
+        }
+if(dp[index][target] != -1)
+{
+    return dp[index][target];
+}
+bool notTake = helper(index-1, target, nums,dp);
+bool take=false;
+if(target>=nums[index])
+{
+    take = helper(index-1, target-nums[index], nums,dp);
+}
+return dp[index][target] = take||notTake;
+    }
 public:
-bool helper(int index,int target,vector<int> &nums,vector<vector<int>> &dp)
-{
-if(target == 0) return true;
-if(index == 0)    return (nums[0] == target);
-if(dp[index][target]!=-1) return dp[index][target];
-bool notTake = helper(index-1,target,nums,dp);
-bool take = false;
-if(target >= nums[index])
-{
-take = helper(index-1,target-nums[index],nums,dp);
-}
-return dp[index][target] = take|notTake;
-}
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum=0;
-for(int i=0;i<n;i++)
-{
-sum += nums[i];
-}
-if(sum % 2 !=0) return false;
- 
-int target = sum/2;
-vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
-return helper(n-1,target,nums,dp);
+ int n = nums.size();
+ int sum=0;
+ for(int i= 0;i<n;i++)
+ {
+    sum += nums[i];
+ }
+ if(sum % 2 !=0 )
+ {
+    // sum is odd 
+    return false;
+ }
+int target = sum/2;;
+vector<vector<int>> dp(n,vector<int>(target+1,-1));
+return helper(n-1,target, nums,dp);
     }
 };
