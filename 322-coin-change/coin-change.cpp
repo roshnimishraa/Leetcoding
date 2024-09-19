19a6bde04fdca77177d0a3b162708b173fbd46ae
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-19 22:21:54 +0530

    Time: 40 ms (48.06%) | Memory: 20.6 MB (49.01%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,31 +1,45 @@
 class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
-  for(int tar=0;tar<=amount;tar++)
-  {
-    if(tar%coins[0] == 0)
- dp[0][tar] = tar/coins[0];
- else
- dp[0][tar] = 1e9;
-   }
-for(int i=1;i<n;i++)
+    int helper(vector<int> &coins,int amount)
+    {
+          int n = coins.size();
+     vector<int> prev(amount+1, 0);
+     vector<int> curr(amount+1,0);
+
+for(int tar=0;tar<=amount;tar++)
 {
- for(int tar=0;tar<=amount;tar++)
- {
-   int notTake = 0+dp[i-1][tar];
-    int take = INT_MAX;
-if(coins[i]<=tar)
-{
-    take = 1 + dp[i][tar-coins[i]];
+    if(tar % coins[0] == 0){
+       prev[tar] = tar/coins[0];
+    }
+    else{
+        prev[tar] = 1e9;
+    }
 }
- dp[i][tar] = min(take,notTake);
+for(int index=1;index<n;index++)
+{
+    for(int tar=0;tar<=amount;tar++)
+    {
+    int notTake = 0 + prev[tar];
+    int take = INT_MAX;
+if(coins[index] <= tar)
+{
+    take = 1 + curr[tar-coins[index]];
+}
+curr[tar] = min(take,notTake);
+    }
+    prev = curr;
+}
+
+int ans= prev[amount];
+if(ans >= 1e9){
+    return -1;
+}
+return ans;
  }
 
-}
-int ans= dp[n-1][amount];
-if(ans >= 1e9) return -1;
-return ans;
+public:
+    int coinChange(vector<int>& coins, int amount) {
+      int n= coins.size();
+       return helper(coins,amount);
+       
     }
 };
