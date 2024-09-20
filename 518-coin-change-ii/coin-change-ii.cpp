1d9706d8a62095ad877141109025ec9b1df5734e
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-20 14:17:37 +0530

    Time: 3 ms (91.55%) | Memory: 20.2 MB (48.26%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,46 +1,28 @@
 class Solution {
     private:
-int helper(int index,int target,vector<int> &coins,vector<vector<int>> &dp)
-{
-// base case 
-if(index == 0){
-    if(target % coins[0] == 0) return 1;
-    else return 0;
-}
-if(dp[index][target]!=-1) return dp[index][target];
-
-int notTake = helper(index-1,target,coins,dp);
-int take=0;
-if(coins[index] <= target) 
-{
-take = helper(index,target-coins[index],coins,dp);
-}
-return dp[index][target]=(take+notTake);
-}
+    int solve(int index, int amount, vector<int> &coins,
+    vector<vector<int>> &dp)
+    {
+        if(index == 0)
+        {
+            return amount % coins[0] == 0;
+ }
+ if(dp[index][amount] != -1){
+    return dp[index][amount];
+ }
+    int notTake = solve(index-1, amount, coins,dp);
+    int take=0;
+    if(coins[index] <= amount)
+    {
+        take = solve(index, amount-coins[index], coins,dp);
+    }
+    return dp[index][amount]= take+notTake;
+    
+    }
 public:
     int change(int amount, vector<int>& coins) {
-int n = coins.size();
-vector<vector<int>> dp(n,vector<int>(amount+1,0));
-// base case 
-// target == 0
-for(int tar=0;tar<=amount;tar++)
-{
-dp[0][tar] = (tar % coins[0]==0);
-}
-
-for(int i=1;i<n;i++)
-{
-for(int tar=0;tar<=amount;tar++)
-{
-int notTake =dp[i-1][tar];
-int take=0;
-if(coins[i]<=tar){
-    take = dp[i][tar-coins[i]];
-}
-
-dp[i][tar] = take+notTake;
-}
-}
-return dp[n-1][amount];
+        int n=coins.size();
+    vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        return solve(n-1, amount, coins,dp);
     }
 };
