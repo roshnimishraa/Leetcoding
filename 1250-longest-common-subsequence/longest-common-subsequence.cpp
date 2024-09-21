5a2906ba99ecd4533174561a1fbc2ff81d6ac0b3
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-21 22:35:25 +0530

    Time: 25 ms (55.55%) | Memory: 26.1 MB (33.58%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,30 +1,38 @@
 class Solution {
-    int solve(int index1,int index2, string &text1, string &text2,
-    vector<vector<int>> &dp)
-    {
-        if(index1 < 0 || index2 < 0)
-        {
-            return 0;
-        }
-    if(dp[index1][index2] != -1)
-    {
-        return dp[index1][index2];
-    }
-    // match 
-if(text1[index1] == text2[index2])
+ private:
+int solve(string text1, string text2)
 {
-    return 1 + solve(index1-1, index2-1, text1, text2,dp);
-}
- // not match
-return dp[index1][index2] = 0 + max(solve(index1-1, index2, text1, text2, dp), solve(index1,index2-1, text1, text2, dp));
+    int n =text1.size();
+    int m = text2.size();
+    vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
 
+// base case -> i<0 || j<0 but in tabulation dp[-1] is unreachable
+// Therefore do shifting of index in order to write tabulation 
+// now the base case acc. to tab. if(i==0 || j==0) 
+ for(int i=0;i<=n;i++)
+ {
+   dp[i][0] = 0;
+ }
+ for(int i=0;i<=m;i++)
+ {
+    dp[0][i] = 0;
+ }
+ for(int index1=1;index1<=n;index1++)
+ {
+    for(int index2=1;index2<=m;index2++)
+    {
+        // matching 
+        if(text1[index1-1] == text2[index2-1])
+    dp[index1][index2] = 1+dp[index1-1][index2-1];
+    // not matching 
+    else
+    dp[index1][index2] = 0 + max(dp[index1-1][index2], dp[index1][index2-1]);
     }
+ }
+ return dp[n][m];
+}
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-    vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        return solve(n-1,m-1,text1,text2, dp);
-
-    }
+       return solve(text1, text2);
+  }
 };
