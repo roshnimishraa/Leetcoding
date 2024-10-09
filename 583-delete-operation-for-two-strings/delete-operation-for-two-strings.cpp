9b13f30b4bb2603e07d19c2432b4ac3d9b5a1e00
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-09 11:16:42 +0530

    Time: 7 ms (90.48%) | Memory: 16.6 MB (27.18%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,42 +1,30 @@
 class Solution {
-    int longestCommonSubsequence(string s,string t)
+    private:
+    int lcs(int ind1, int ind2, string &s1, string &s2, vector<vector<int>> &dp)
     {
-int n = s.length();
-int m = t.length();
-vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-// base case
-for(int j=0;j<m+1;j++)
-{
-    dp[0][j] = 0;
-}
-for(int i=0;i<n+1;i++)
-{
-    dp[i][0] = 0;
-}
-for(int i=1;i<n+1;i++)
-{
-    for(int j=1;j<m+1;j++)
-    {
-    if(s[i-1] == t[j-1])
-dp[i][j] = 1 + dp[i-1][j-1];
-  
-    else
-dp[i][j] = 0 + max(dp[i-1][j],dp[i][j-1]);
-    
-}
-}
-return dp[n][m];
-}
+        if(ind1 <0 || ind2<0)
+        {
+            return 0;
+        }
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
+        }
+        // match
+        if(s1[ind1] == s2[ind2])
+        {
+        return dp[ind1][ind2] = 1+lcs(ind1-1, ind2-1, s1,s2,dp);
+        }
 
-    
+        // not match 
+        return dp[ind1][ind2] = 0+ max(lcs(ind1,ind2-1,s1,s2,dp), lcs(ind1-1,ind2, s1, s2,dp));
+    }
 public:
     int minDistance(string word1, string word2) {
-int lcs=longestCommonSubsequence(word1,word2);
-int n = word1.length();
-int m = word2.length();
-//int deletion = n - lcs;
-//int insertion = m - lcs;
-int ans = n + m - 2 * lcs;
-return ans;    
+        int n = word1.size();
+        int m =word2.size();
+        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+    int LCS = lcs(n-1, m-1, word1, word2,dp);
+    int ans= n+m - 2*LCS;
+    return ans;
     }
 };
