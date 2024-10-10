ae5603ec0bb273f37aad7f4cf383ccfff65ebefe
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-10 21:29:15 +0530

    Time: 49 ms (20.06%) | Memory: 16.2 MB (15.87%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
-  vector<int> dp(n,1), count(n,1);
-  int maxi=1;
-    for(int i= 0;i<n;i++)
+        int n=nums.size();
+     vector<int> dp(n,1);   
+     vector<int> count(n,1);
+     int maxi=1;
+     for(int index=0;index<n;index++)
+     {
+        for(int prev_index=0; prev_index<index;prev_index++)
+        {
+    if(nums[prev_index]<nums[index] && dp[prev_index]+1 > dp[index])
     {
-    for(int prev=0;prev<i;prev++)
-    {
-if(nums[prev] < nums[i] && 1+dp[prev] > dp[i])
-{
-    dp[i] = 1+dp[prev];
-    count[i] = count[prev];
-}
-else if(nums[prev] < nums[i] && 1+dp[prev] == dp[i])
-    {
-        count[i] += count[prev];
+        dp[index] = dp[prev_index] + 1;
+         // Inherit count
+         count[index] = count[prev_index];
     }
-}
-maxi =max(maxi,dp[i]);
-    }
-int nos=0;
-for(int i=0;i<n;i++)
-{
-    if(dp[i] == maxi)
+    else if(nums[prev_index] < nums[index] && dp[prev_index]+1 == dp[index])
     {
-nos += count[i];
+        count[index] = count[index]+count[prev_index];
     }
-}
-   
-return nos;
+        }
+        maxi = max(dp[index],maxi);
+     }
+     int noOfLIS = 0;
+     for(int i=0;i<n;i++)
+     {
+        if(dp[i] == maxi)
+        {
+noOfLIS += count[i];
+        }
+     }
+     return noOfLIS;
     }
 };
