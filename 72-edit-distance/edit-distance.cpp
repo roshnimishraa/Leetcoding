b0bb0331b41e351dbdd9443241974cc718e3cf3c
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-10 16:57:58 +0530

    Time: 9 ms (50.85%) | Memory: 12.9 MB (65.09%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,34 +1,43 @@
 class Solution {
     private:
-    int solve(int i, int j, string &word1, string &word2,
-    vector<vector<int>> &dp)
+    int solve(string &word1, string &word2)
     {
-        // base case
-        if(i<0) {
-            return j+1; //no. of insert operations
-        }
-        if(j<0){
-            return i+1; //no. of delete operations
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        // match
-        if(word1[i] == word2[j])
+         int n = word1.size();
+ int m = word2.size();
+vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+// base case i==0 || j==0
+for(int i=0; i<=n;i++)
+{
+dp[i][0] = i;
+}
+for(int j=0;j<=m;j++)
+{
+dp[0][j] = j;
+}
+     
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=m; j++)
         {
-            return dp[i][j] = 0+solve(i-1,j-1,word1,word2,dp);
+ // match
+        if(word1[i-1] == word2[j-1])
+        {
+             dp[i][j] = 0+ dp[i-1][j-1];
         }
+        else {
         // not match
-int insert = 1 + solve(i, j-1,word1,word2,dp);
-int deletion = 1 + solve(i-1,j,word1,word2,dp);
-int replace = 1 + solve(i-1,j-1,word1,word2,dp);
-return dp[i][j] = min(insert,min(deletion,replace));
+int insert = 1 + dp[i][j-1];
+int deletion = 1 + dp[i-1][j];
+int replace = 1 + dp[i-1][j-1];
+ dp[i][j] = min(insert,min(deletion,replace));
+        }
+        }
+    }
+       return dp[n][m];
     }
 public:
     int minDistance(string word1, string word2) {
- int n = word1.size();
- int m = word2.size();
-vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,word1,word2,dp);
+
+      return solve(word1,word2);
     }
 };
