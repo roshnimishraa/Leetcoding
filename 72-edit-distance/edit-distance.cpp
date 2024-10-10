e23beafb3a1b5992127a5972bb4599a6cd31f955
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-10 17:02:45 +0530

    Time: 6 ms (71.55%) | Memory: 10.4 MB (94.59%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,36 +4,37 @@ class Solution {
     {
          int n = word1.size();
  int m = word2.size();
-vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-// base case i==0 || j==0
-for(int i=0; i<=n;i++)
-{
-dp[i][0] = i;
-}
+vector<int> prev(m+1,0);
+vector<int> curr(m+1,0);
+
+
 for(int j=0;j<=m;j++)
 {
-dp[0][j] = j;
+prev[j] = j;
 }
      
     for(int i=1; i<=n; i++)
     {
+        // Initialize the first column of the current row
+        curr[0] = i;
         for(int j=1; j<=m; j++)
         {
  // match
         if(word1[i-1] == word2[j-1])
         {
-             dp[i][j] = 0+ dp[i-1][j-1];
+             curr[j] = 0+ prev[j-1];
         }
         else {
         // not match
-int insert = 1 + dp[i][j-1];
-int deletion = 1 + dp[i-1][j];
-int replace = 1 + dp[i-1][j-1];
- dp[i][j] = min(insert,min(deletion,replace));
+int insert = 1 + curr[j-1];
+int deletion = 1 + prev[j];
+int replace = 1 + prev[j-1];
+curr[j] = min(insert,min(deletion,replace));
         }
         }
+        prev = curr;
     }
-       return dp[n][m];
+       return prev[m];
     }
 public:
     int minDistance(string word1, string word2) {
