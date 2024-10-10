5dfe34f02d6efd8ef514a47671c51cb2a9758d16
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-10 19:10:50 +0530

    Time: 48 ms (46.99%) | Memory: 12.7 MB (86.99%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,40 +1,62 @@
 class Solution {
     private:
-  bool solve(int i,int j,string &s, string &p,vector<vector<int>> &dp)
+  bool solve(string s, string p)
   {
-    if(i < 0 && j<0) return true;
-    if (i < 0 && j >= 0)
-        return false;
-    if(i>=0 && j<0) 
-    {
-        for(int k=0; k<=i; k++)
+     int n = p.length();
+    int m = s.length();
+vector<bool> prev(m+1,false);
+vector<bool> curr(m+1,false);
+
+// case 1
+prev[0] = true;
+// case 2
+for(int j=1;j<=m;j++){
+ prev[j] = false;
+ }
+for(int i=1;i<=n;i++)
+{
+    bool flag = true;
+     for(int k=1; k<=i; k++)
         {
-            if(p[k] != '*'){
-            return false;
+            if(p[k-1] != '*'){
+                flag = false;
+                break;
             }
         }
-        return true;
-    }
-if(dp[i][j] != -1){
-    return dp[i][j];
+      curr[0] = flag;
 }
-    // match
-    if(p[i] == s[j] || p[i] == '?')
+ 
+for(int i=1;i<=n;i++){
+	        // case 3
+            bool flag = true;
+            for(int k=1;k<=i;k++){
+                if(p[k-1] != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            curr[0] = flag;
+    for(int j=1;j<=m;j++)
     {
-        return dp[i][j] = solve(i-1, j-1, s, p,dp);
+ // match
+    if(p[i-1] == s[j-1] || p[i-1] == '?')
+    {
+         curr[j] = prev[j-1];
     }
-    else if(p[i] == '*')
+    else if(p[i-1] == '*')
     {
         // empty || matches 
-return dp[i][j]= solve(i-1,j,s,p,dp) || solve(i,j-1,s,p,dp);
+curr[j]= prev[j] || curr[j-1];
     }
-    return dp[i][j] = false;
+    else
+    curr[j] = false;
+    }
+    prev = curr;
+}
+   return prev[m];
   }
 public:
-    bool isMatch(string s, string p) {
-        int n = p.length();
-        int m = s.length();
-    vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        return solve(n-1,m-1,s,p,dp);
+    bool isMatch(string s, string p) {      
+ return solve(s,p);
     }
 };
