d6d553c85cfd518b9146045589ac6bcd02268e43
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-12 16:47:23 +0530

    Time: 4 ms (33.80%) | Memory: 8.1 MB (52.20%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,29 +1,33 @@
 class Solution {
+    private:
+    int solve(string &s)
+    {
+           int n = s.length();
+    vector<int> dp(n+1,0);
+     dp[n] = 1;
+    // leading zero
+   for(int i=n-1;i>=0;i--){
+    if(s[i] == '0')
+    {
+        dp[i] = 0;
+    }
+    else{
+        int op1 = dp[i+1];
+  int op2=0;
+    if(i+1 < s.length()){
+        if(s[i] == '1' || s[i] == '2' && s[i+1] <='6')
+        {
+            op2 = dp[i+2];
+        }
+    }
+    dp[i] = op1+op2;
+    }
+   }
+   
+ return dp[0];
+    }
 public:
     int numDecodings(string s) {
-        int n=s.length();
-        vector<int> dp(n+1,0);
-    dp[n] = 1; //if reached till last index that means 
-            //1 valid solution is found
-for(int i=n-1;i>=0;i--)
-{
- if(s[i]=='0') 
- {
-  dp[i] = 0;
- }
- else{
-int op1= dp[i+1];
-int op2=0;
-if(i+1<n)
-{
-if(s[i]=='1'||(s[i]=='2' && s[i+1]<='6'))
-{
- op2= dp[i+2];
-}
-}
-dp[i] = op1+op2;
- }
-} 
-return dp[0];
+    return solve(s);
     }
 };
