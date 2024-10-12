6295f942c6ef0fcd77cec797d306bd7dd5be3095
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-12 12:29:02 +0530

    Time: 28 ms (36.24%) | Memory: 11.3 MB (17.13%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -3,39 +3,36 @@ public:
     string minWindow(string s, string t) {
         int n = s.length();
         map<char, int> mp;
-        
-        for(char &ch : t) {
+        for(char &ch : t)
+        {
             mp[ch]++;
         }
-        
         int requiredCount = t.length();
-        int i = 0, j  = 0;
-        int minStart  = 0;
+        int i=0, j=0;
+        int minStart = 0;
         int minWindow = INT_MAX;
-        while(j < n) {
-            char ch_j = s[j];
-            if(mp[ch_j] > 0)
-                requiredCount--;
-            
-            mp[ch_j]--;
-            
-            while(requiredCount == 0) { //shrink the window
-                if(minWindow > j-i+1) {
-                    minWindow = j-i+1;
-                    minStart  = i;
-                }
-                
-                char ch_i = s[i];
-                mp[ch_i]++;
-                if(mp[ch_i] > 0)
-                    requiredCount++;
-                i++;
-            }
-            
-            j++; 
+    while(j<n)
+    {
+        char ch_j = s[j];
+        if(mp[ch_j] > 0){
+            requiredCount--;
         }
-        
-        return minWindow == INT_MAX ? "" : s.substr(minStart, minWindow);
-   
+        mp[ch_j]--;
+
+        while(requiredCount == 0){
+            if(minWindow > j-i+1)
+            {
+                minWindow = j-i+1;
+                minStart = i;
+            }
+            char ch_i = s[i];
+            mp[ch_i]++;
+            if(mp[ch_i] > 0)
+            requiredCount++;
+            i++;
+        }
+        j++;
+    }
+    return minWindow == INT_MAX ? "": s.substr(minStart, minWindow);
     }
 };
