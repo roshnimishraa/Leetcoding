aee3861b3b67d95e3d07d95d46bc248c72eaf8f1
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-15 14:55:29 +0530

    Time: 3 ms (20.19%) | Memory: 7.4 MB (61.17%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -1,11 +1,32 @@
 class Solution {
 public:
     int nextGreaterElement(int n) {
-        string str = to_string(n);
-        next_permutation(str.begin() , str.end());
-        long long int ans = stoll(str);
-        if(ans > INT_MAX || ans <= n)
-            return -1;
-        return ans;
+        string s = to_string(n);
+        for(int i=s.size()-2 ; i>=0 ; i--)
+        {
+            int pos = -1;
+            for(int j=i+1 ; j<s.size() ; j++)
+            {
+                if(s[j] > s[i])
+                {
+                    if(pos == -1) pos = j;
+                    else if(s[j] < s[pos]) pos = j;      
+                }
+            }
+            
+            if(pos == -1) continue;
+            
+            swap(s[i] , s[pos]);
+            sort(s.begin() + i + 1 , s.end());
+            long long int val = stoll(s);
+            if(val > INT_MAX) 
+                return -1;
+            else
+                return val;
+        }
+        return -1;
+        
     }
 };
+
+      
