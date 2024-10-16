e174581e723d9c2bd3078a491239048f87270785
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-16 19:58:53 +0530

    Time: 88 ms (52.68%) | Memory: 111.3 MB (65.16%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-       int fuel =0;
-       int ans=0;
-    int gasSum =0;   
-for(int i=0;i<gas.size();i++)
-{
- gasSum += gas[i];
-}
-int costSum =0;
-for(int i=0;i<cost.size();i++)
-{
-    costSum += cost[i];
-}
-// base case 
-if(gasSum < costSum) return -1;
-for(int i=0;i<gas.size();i++)
-{
-    fuel += gas[i]-cost[i];
- if(fuel<0)
- {
-     ans = i+1;
- fuel = 0;
- }
-}
-return ans;
+        int fuel=0, total=0;
+        int ans=0;
+    for(int i=0;i<gas.size();i++){
+         fuel += gas[i] - cost[i];
+        total += gas[i] - cost[i];
+        if(fuel < 0){
+            ans = i+1;
+            fuel=0;
+        }
+    }
+    return (total >= 0) ? ans : -1;
     }
 };
