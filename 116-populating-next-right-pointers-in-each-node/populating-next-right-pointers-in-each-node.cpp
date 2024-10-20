5c9940a0bce9e2399576d4b19c00682726d80330
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-20 21:12:56 +0530

    Time: 16 ms (38.91%) | Memory: 19.1 MB (40.92%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -20,16 +20,29 @@ class Solution {
 public:
     Node* connect(Node* root) {
         if(root == NULL) return root;
-    if(root->left != NULL)
-    {
-        root->left->next = root->right;
-    }
-    if(root->right != NULL && root->next != NULL)
-    {
- root->right->next = root->next->left;
-    }
-    connect(root->left);
-    connect(root->right);
-    return root;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n = q.size();
+            for(int i=0;i<n;i++)
+            {
+                Node* curr = q.front();
+                q.pop();
+                if(i != n-1) 
+                {
+                    curr->next = q.front();
+                }
+                if(curr->left != NULL)
+                {
+                    q.push(curr->left);
+                }
+                if(curr->right != NULL)
+                {
+                    q.push(curr->right);
+                }
+            }
+        }
+        return root;
     }
 };
