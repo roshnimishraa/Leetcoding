533c9496df384327f7497f66dddaebc6387d822d
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-24 21:48:08 +0530

    Time: 8 ms (94.07%) | Memory: 19 MB (47.20%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,45 +1,42 @@
 class Solution {
-public:
-    bool canFinish(int N,  vector<vector<int>>& prerequisites)
+    private:
+    bool isCycleDFS(vector<int> adj[], vector<int> &vis, vector<int> &pathVis, int node)
     {
-        vector<int> adj[N];
-    for(auto it:prerequisites)
-    {
-   
-   adj[it[0]].push_back(it[1]);
-    }
-// kahn's algo 
-vector<int> indegree(N,0);
-for(int i=0;i<N;i++)
-{
-    for(auto it:adj[i])
-    {
-    indegree[it]++;
-    }
-}
-queue<int> q;
-for(int i=0;i<N;i++)
-{
-    if(indegree[i]==0){
-q.push(i);
-    }
-}
+        vis[node] = 1;
+        pathVis[node] = 1;
+        for(auto it: adj[node])
+        {
+            if(vis[it] == 0)
+            {
+         // If a cycle is detected in DFS
+                if(isCycleDFS(adj,vis,pathVis,it))
+                return true;
+            }
+// If the neighbor is already being visited (back edge), cycle detected
+        else{
+       if(pathVis[it] == 1) return true;
+        }
 
-int count=0;
-while(!q.empty()){
-    auto node = q.front();
-    q.pop();
-count++;
-for(auto it:adj[node])
-{
-    indegree[it]--;
-    if(indegree[it]==0)
-    {
-        q.push(it);
+        }
+        pathVis[node] = 0;
+        return false;
     }
-}
-}
-if(count == N) return true;
-return false;
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+       vector<int> adj[numCourses];
+       for(auto it: prerequisites)
+       {
+        int u = it[0];
+        int v = it[1];
+        adj[v].push_back(u);
+       }
+       vector<int> vis(numCourses,0);
+       vector<int> pathVis(numCourses,0);
+       for(int i=0;i<numCourses;i++)
+       {
+        if(isCycleDFS(adj,vis,pathVis,i))
+        return false;
+       }
+       return true;
     }
 };
