530899b9c48e378d9cb0eb4734cc628fa664fc37
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-26 00:03:41 +0530

    Time: 3 ms (96.85%) | Memory: 27 MB (66.72%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,37 +1,45 @@
 class Solution {
-    int dfs(vector<vector<int>> &grid,int i,int j,int &count)
+    private:
+    void dfs(vector<vector<int>> &grid, int row, int col, int &count)
     {
         int n = grid.size();
         int m = grid[0].size();
-    if(i>=0 && i<n && j>=0 && j<m && grid[i][j]==1)
+ int dx[] = {-1, 1, 0, 0};
+ int dy[] = {0, 0, 1, -1};
+
+    count += 1; //increment count
+
+    grid[row][col] = 0; // mark cell as visited
+
+    for(int i=0;i<4;i++)
     {
-        grid[i][j] = 0;
-        count++;
-          dfs(grid,i+1,j,count);
-    dfs(grid,i-1,j,count);
-    dfs(grid,i,j+1,count);
-    dfs(grid,i,j-1,count);
+        int nrow = row + dx[i];
+        int ncol = col + dy[i];
+
+    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
+    grid[nrow][ncol] == 1)
+    {
+        dfs(grid, nrow, ncol, count);
     }
-  
-    return count;
+    }
     }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-      int n = grid.size();
-      int m = grid[0].size();
-      int ans=0;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
+        int n = grid.size();
+        int m = grid[0].size();
+ int maxArea =0;
+        for(int i=0;i<n;i++)
         {
-            if(grid[i][j]==1)
+            for(int j=0;j<m;j++)
             {
-                int count=0;
-        ans = max(ans,dfs(grid,i,j,count));
+         if(grid[i][j] == 1)
+         {
+            int area = 0;
+            dfs(grid,i,j,area);
+            maxArea = max(maxArea, area);
+         }
             }
         }
-    }  
-
-    return ans;
+        return maxArea;
     }
 };
