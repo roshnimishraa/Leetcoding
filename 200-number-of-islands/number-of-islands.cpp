2f4a79b0624f82bf14b35e7a25ea772fdb555398
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-26 13:09:58 +0530

    Time: 28 ms (60.20%) | Memory: 17.1 MB (52.12%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,40 +1,38 @@
 class Solution {
-public:
-void dfs(int row,int col,vector<vector<char>> &grid,vector<vector<int>> &vis)
-{
-      int n=grid.size();
-    int m = grid[0].size();
-vis[row][col]=1;
-int dx[]={-1,0,1,0}; 
-int dy[]={0,1,0,-1};          
-for(int i=0;i<4;i++)
-{
-    int x=row+dx[i];
-    int y=col+dy[i];
-if(x>=0 && x<n && y>=0 && y<m && !vis[x][y] &&
- grid[x][y]=='1')
-{
-dfs(x,y,grid,vis);
-}
-}
-}
-    int numIslands(vector<vector<char>>& grid)
+    void dfs(int row, int col,vector<vector<char>> &grid,vector<vector<int>> &vis)
     {
-    int n=grid.size();
-    int m = grid[0].size();
-vector<vector<int>> vis(n,vector<int>(m,0));
-int count=0;
-for(int i=0;i<n;i++)
-{
-    for(int j=0;j<m;j++)
-{
-    if(!vis[i][j] &&  grid[i][j]=='1')
+        int n = grid.size();
+        int m = grid[0].size();
+        vis[row][col] = 1;
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
+        for(int i=0;i<4;i++)
+        {
+            int nrow = row + dx[i];
+            int ncol = col + dy[i];
+    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol] == '1')
     {
- dfs(i,j,grid,vis);
-         count++;
+        dfs(nrow, ncol, grid, vis);
     }
-}
-}
-return count;
+        }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int> (m,0));
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(!vis[i][j] && grid[i][j] == '1')
+                {
+                    dfs(i,j, grid, vis);
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 };
