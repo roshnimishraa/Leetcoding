cfec952a4e417e726a2eb08361eec5ae7550223b
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-27 15:53:02 +0530

    Time: 6 ms (84.92%) | Memory: 41.4 MB (95.26%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,36 +1,70 @@
-class Solution {
-    void dfs(vector<vector<int>> &adj, vector<bool> &vis, int src)
-    {
-        vis[src] = true;
-        for(int i: adj[src])
-        {
-            if(!vis[i])
-            dfs(adj, vis, i);
+class DisjointSet{
+    public:
+    vector<int> parent, size;
+
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1);
+
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+            size[i] = 1;
         }
     }
+
+    int findUPar(int node){
+        if(node == parent[node]){
+            return node;
+        }
+    return parent[node] = findUPar(parent[node]);
+    }
+
+
+void unionBySize(int u, int v)
+{
+    int ulp_u = findUPar(u);
+    int ulp_v = findUPar(v);
+
+    if(ulp_u == ulp_v) return;
+    
+    if(size[ulp_u] < size[ulp_v]){
+        parent[ulp_u] = ulp_v;
+        size[ulp_v] += size[ulp_u];
+    }
+    else{
+        parent[ulp_v] = ulp_u;
+        size[ulp_u] += size[ulp_v];
+    }
+}
+};
+class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
- // we need n-1 edges atleast for n vertices, if connection size is lesser means we don't have sufficient edge 
- if(connections.size() < n-1)
- {
-    return -1;
- }
- vector<vector<int>> adj(n);
- for(auto it: connections)
- {
-     adj[it[0]].push_back(it[1]);
-     adj[it[1]].push_back(it[0]);
- }
- vector<bool> vis(n,false);
+        DisjointSet ds(n);
+        int countExtra = 0;
+    for(auto &it: connections){
+        int u = it[0];
+        int v = it[1];
+    // both have same parent and this is extra edge
+    if(ds.findUPar(u) == ds.findUPar(v))
+    {
+        countExtra++;
+    }
+    else{
+        ds.unionBySize(u,v);
+    }
+    }
+    int components = 0 ;
+    for(int i=0;i<n;i++){
+        if(ds.parent[i] == i){
+            components++;
+        }
+    }
 
- int components = 0;
- for(int i=0;i<n;i++)
- {
-    if(!vis[i]){
-        dfs(adj, vis, i);
-        components++;
-    }
- }
- return components-1;
-    }
+    int ans = components-1;
+   if(countExtra < ans){
+    return -1;
+   }
+   return ans;
+    } 
 };
